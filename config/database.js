327aba3ff12e4c0c842185127c936c68bb78ec1a
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-01 07:25:37 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB connected");
+
   } catch (error) {
-    console.log(error);
+
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+
   }
 };
 
