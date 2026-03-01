6e83a8af522780d56735f74106cae1428ce9b20e
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-01 12:00:26 +0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,30 +2,38 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 
-router.get("/", async(req,res)=>{
 
-const products = await Product.find();
-
-res.json(products);
-
+// GET all products
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.post("/", async(req,res)=>{
 
-const product = new Product(req.body);
-
-await product.save();
-
-res.json(product);
-
+// ADD product
+router.post("/", async (req,res)=>{
+  try{
+    const product = new Product(req.body);
+    await product.save();
+    res.json(product);
+  }catch(err){
+    res.status(500).json({error: err.message});
+  }
 });
 
-router.delete("/:id", async(req,res)=>{
 
-await Product.findByIdAndDelete(req.params.id);
-
-res.json({message:"Product deleted"});
-
+// DELETE product
+router.delete("/:id", async (req,res)=>{
+  try{
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({message:"Product deleted"});
+  }catch(err){
+    res.status(500).json({error: err.message});
+  }
 });
 
 module.exports = router;
