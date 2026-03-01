f5f00402d5933d0431ca75f3aac92c1e9785e060
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-01 13:27:49 +0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,49 +2,54 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 
+const multer = require("multer");
+const cloudinary = require("../config/cloudinary");
 
-// GET all products
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+const storage = multer.memoryStorage();
+const upload = multer({storage});
+
+router.get("/", async(req,res)=>{
+  const products = await Product.find();
+  res.json(products);
+});
+
+router.post("/", upload.single("image"), async(req,res)=>{
+  try{
+
+    let imageUrl = "";
+
+    if(req.file){
+      const result = await cloudinary.uploader.upload_stream(
+        {folder:"shopsphere"},
+        async(error,result)=>{
+
+          if(error) return res.status(500).json(error);
+
+          imageUrl = result.secure_url;
+
+          const product = new Product({
+            name:req.body.name,
+            price:req.body.price,
+            category:req.body.category,
+            image:imageUrl
+          });
+
+          await product.save();
+
+          res.json(product);
+
+        }
+      ).end(req.file.buffer);
+    }
+
+  }catch(err){
+    res.status(500).json({error:err.message});
   }
 });
 
-
-// ADD product
-router.post("/", async (req,res)=>{
-  try{
-
-    const {name, price, category, image} = req.body;
-
-    const product = new Product({
-      name,
-      price,
-      category,
-      image
-    });
-
-    await product.save();
-
-    res.json(product);
-
-  }catch(err){
-    res.status(500).json({error: err.message});
-  }
-});
-
-
-// DELETE product
-router.delete("/:id", async (req,res)=>{
-  try{
-    await Product.findByIdAndDelete(req.params.id);
-    res.json({message:"Product deleted"});
-  }catch(err){
-    res.status(500).json({error: err.message});
-  }
+router.delete("/:id", async(req,res)=>{
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({message:"Product deleted"});
 });
 
 module.exports = router;
