dd1eb1e0b369e00cbd1e45865048a3698721b9ef
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-01 20:22:37 +0300

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.deckqjqrp,
-  api_key: process.env.932553454141212,
-  api_secret: process.env.Le46iQMDvXdIG60aXngS_yO-eIc
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
 });
 
 module.exports = cloudinary;
