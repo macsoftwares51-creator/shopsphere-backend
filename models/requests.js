7a54e4a8b5b5ddb461790760e00dabd97145d81a
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-04 20:01:39 +0300

    Update requests.js

--- models/requests.js
+++ models/requests.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  product: String,
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  product: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: "Pending" // Pending | Added
+  },
   createdAt: {
     type: Date,
     default: Date.now
