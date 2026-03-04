a2bac593ff1da60f45ce2ac5ebb081e7f08b25aa
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-04 11:28:40 +0300

    Update createadmin.js

--- createadmin.js
+++ createadmin.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const Admin = require("./models/Admin"); // path to your Admin schema
 
 async function createAdmin() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/yourDB"); // replace yourDB with your DB name
+  await mongoose.connect("mongodb://127.0.0.1:27017/Mac");
 
   const admin = new Admin({
-    username: "admin",              // choose your username
-    password: "MySecurePassword123" // choose your password
+    username: "mac",             
+    password: "Godisable" 
   });
 
-  await admin.save(); // will hash password automatically
+  await admin.save(); 
   console.log("Admin created successfully!");
 
   mongoose.disconnect();
