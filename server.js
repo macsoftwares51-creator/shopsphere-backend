4e41234e7467985bc4c135a8a89a5708e4669289
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-04 17:20:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,16 @@ const express = require("express");
 const cors = require("cors");
 
 const connectDB = require("./config/database");
-
 const productRoutes = require("./routes/products");
+
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+
 const app = express();
+
 /* ---------- CONNECT DATABASE ---------- */
 connectDB();
-app.post("/admin-login", (req, res) => {
-  const { password } = req.body;
 
-  if (password === ADMIN_PASSWORD) {
-    res.json({ success: true });
-  } else {
-    res.json({ success: false });
-  }
-});
-/* ---------- MIDDLEWARE ---------- */
+/* ---------- MIDDLEWARE (MUST COME FIRST) ---------- */
 app.use(cors({
   origin: "https://macsoftwares51-creator.github.io",
   methods: ["GET","POST","PUT","DELETE"],
@@ -30,11 +24,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* ---------- ROUTES ---------- */
+
+app.post("/admin-login", (req, res) => {
+  const { password } = req.body;
+
+  if (password === ADMIN_PASSWORD) {
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
+});
+
 app.use("/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("ShopSphere API running");
 });
+
 /* ---------- SERVER ---------- */
 const PORT = process.env.PORT || 10000;
 
