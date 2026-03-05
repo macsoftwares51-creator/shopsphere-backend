80322e4774fc53d5da21a539351b38ba2c351e5c
maxwell mwathiki <macsoftwares51@gmail.com>
2026-03-05 06:20:46 +0300

    Update SellerRequest.js

--- routes/SellerRequest.js
+++ routes/SellerRequest.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Seller = require("../models/SellerRequest");
 
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const CloudinaryStorage = require("multer-storage-cloudinary").CloudinaryStorage;
 const cloudinary = require("cloudinary").v2;
 
 /* CLOUDINARY STORAGE */
