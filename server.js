153c64c974bafdb37b78170db1dc468e32159f6b
maxwell mwathiki <macsoftwares51@gmail.com>
2026-04-20 19:52:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({{ limit: '50mb', extended: true }));
 app.use("/requests", requestRoutes);
 app.use("/Review", reviewRoutes);
 app.use("/SellerRequest", SellerRequestRoutes);
